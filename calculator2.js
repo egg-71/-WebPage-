c2f526c9961c33114a3dbe13ca5a9b1c9002ec05
egg-71 <61447217+egg-71@users.noreply.github.com>
2021-03-22 21:14:28 +0800

    Add files via upload

---
+++ calculator2.js
@@ -0,0 +1,16 @@
+function Delete(){
+	document.getElementById("display").value="";
+}
+
+function get (value){
+	document.getElementById("display").value+=value;
+}
+
+function calculates(){
+	var result=0;
+	result=document.getElementById("display").value;
+	document.getElementById("display").value=" ";
+	document.getElementById("display").value=
+	eval(result);
+	
+} ;
